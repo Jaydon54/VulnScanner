530450974833be82b0dda47cbde2adb1c0af9728
Adham Gohar <125025049+Adham-Gohar@users.noreply.github.com>
2025-04-28 20:33:46 -0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,10 @@
- # utils/utils.py
-# Code for reusable utility functions goes here
-
-# Util #2: port scanner print module
-#vprints all the info of the scan results used in all the scanner funcitons
+# utils/utils.py
 def print_results(scanner):
     for host in scanner.all_hosts():
-        print(f"Host: {host} ({scanner[host].hostname()})") 
-        print(f"State: {scanner[host].state()}") #whether the host is up or down
-        
-        for proto in scanner[host].all_protocols(): 
+        print(f"Host: {host} ({scanner[host].hostname()})")
+        print(f"State: {scanner[host].state()}")
+
+        for proto in scanner[host].all_protocols():
             print(f"Protocol: {proto}")
             ports = scanner[host][proto].keys()
 
@@ -20,8 +16,6 @@ def print_results(scanner):
                 version = port_data.get('version', '')
                 extra_info = f"{product} {version}".strip()
 
-                print(f"Port: {port}\tService: {service}", end='\n')
+                print(f"Port: {port}\tService: {service}")
                 if extra_info:
-                    print(f"\tExtra Info: {extra_info}", end='')
-
-    
+                    print(f"\tExtra Info: {extra_info}")
